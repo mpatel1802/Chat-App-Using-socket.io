0da82ae6b596be690ed4d1287c3c0fd114d9a2b8
Mann Patel <mannpatel2118@gmail.com>
2022-04-09 16:11:21 -0400

    commit

---
+++ src/app/chat-window/chat-window.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ChatService } from '../chat.service';
+
+@Component({
+  selector: 'app-chat-window',
+  templateUrl: './chat-window.component.html',
+  styleUrls: ['./chat-window.component.css']
+})
+export class ChatWindowComponent implements OnInit {
+
+  private getMessagesSub: Subscription | undefined;
+  messages: string[] = [];
+  currentMessage: string = "";
+
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.getMessagesSub = this.chatService.getMessages.subscribe((data: string) => {
+      this.messages.push(data);
+    });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.currentMessage);
+    this.currentMessage = "";
+  }
+
+  ngOnDestroy() {
+    this.getMessagesSub?.unsubscribe();
+  }
+
+}
---
+++ src/app/chat.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Subject } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private socket: Socket | undefined; // The client instance of socket.io
+  public getMessages: Subject<string> = new Subject();
+
+  constructor() {
+
+    this.socket = io('https://blooming-coast-37871.herokuapp.com/');
+
+    this.socket?.on('chat message', (msg: string) => {
+      this.getMessages.next(msg); // send the new message
+    });
+
+  }
+
+  sendMessage(msg: string) {
+    this.socket?.emit('chat message', msg);
+  }
+
+}
